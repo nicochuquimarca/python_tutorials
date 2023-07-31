898e3dd1edb58eb15feea302d5df87449f1ad368
Nicolás Chuquimarca <nicoc991@hotmail.com>
2023-07-30 22:44:57 -0500

    Add my name to the print function

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+#  My first Python program
+print("Hello World") # print function
+a = 1+52             # assignment statement
+a                    # Call the variable
+
+# Now print my full name
+name = "Nicolás Chuquimarca"
+print(name)
+
